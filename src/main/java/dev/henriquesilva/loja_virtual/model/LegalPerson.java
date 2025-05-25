4e7d7d3aac25fc1e7d66348f1942b3c22ff249e3
Henrique Silva <henriquersilva.al@gmail.com>
2025-05-24 23:24:39 -0300

    Pessoa Física corrigido

--- src/main/java/dev/henriquesilva/loja_virtual/model/LegalPerson.java
+++ src/main/java/dev/henriquesilva/loja_virtual/model/LegalPerson.java
@@ -1,4 +1,73 @@
 package dev.henriquesilva.loja_virtual.model;
 
-public class LegalPerson {
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "legal_person")
+@PrimaryKeyJoinColumn(name = "id")
+public class LegalPerson extends Person {
+    private static final long serialVersionUID = 1L;
+
+    @Column(unique = true, nullable = false)
+    private String cnpj;
+    @Column(unique = true)
+    private String stateRegistration;
+    @Column(unique = true)
+    private String municipalRegistration;
+    @Column(nullable = false)
+    private String fantasyName;
+    @Column(nullable = false)
+    private String legalName ;
+    @Column(nullable = false)
+    private String category;
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public String getStateRegistration() {
+        return stateRegistration;
+    }
+
+    public void setStateRegistration(String stateRegistration) {
+        this.stateRegistration = stateRegistration;
+    }
+
+    public String getMunicipalRegistration() {
+        return municipalRegistration;
+    }
+
+    public void setMunicipalRegistration(String municipalRegistration) {
+        this.municipalRegistration = municipalRegistration;
+    }
+
+    public String getFantasyName() {
+        return fantasyName;
+    }
+
+    public void setFantasyName(String fantasyName) {
+        this.fantasyName = fantasyName;
+    }
+
+    public String getLegalName() {
+        return legalName;
+    }
+
+    public void setLegalName(String legalName) {
+        this.legalName = legalName;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
 }
