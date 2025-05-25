92aa5ac1229e7b207ac1bb307cfe9e04647283ef
Henrique Silva <henriquersilva.al@gmail.com>
2025-05-25 00:08:04 -0300

    Pessoa e endereço adicionados (relacionamento)

---
+++ src/main/java/dev/henriquesilva/loja_virtual/model/Address.java
@@ -0,0 +1,122 @@
+package dev.henriquesilva.loja_virtual.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Table(name = "address")
+@SequenceGenerator(name = "seq_address", initialValue = 1, allocationSize = 1)
+public class Address implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_address")
+    private long id;
+    @Column(nullable = false)
+    private String street;
+    @Column(nullable = false)
+    private String city;
+    @Column(nullable = false)
+    private String state;
+    @Column(nullable = false)
+    private String zip;
+    @Column(nullable = false)
+    private String country;
+    private String number;
+    private String complement;
+    private boolean defaultAddress;
+
+    @ManyToOne(targetEntity = Person.class, fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "person_id")
+    @JsonIgnore
+    private Person person;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getComplement() {
+        return complement;
+    }
+
+    public void setComplement(String complement) {
+        this.complement = complement;
+    }
+
+    public boolean isDefaultAddress() {
+        return defaultAddress;
+    }
+
+    public void setDefaultAddress(boolean defaultAddress) {
+        this.defaultAddress = defaultAddress;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Address address = (Address) o;
+        return id == address.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
--- src/main/java/dev/henriquesilva/loja_virtual/model/Person.java
+++ src/main/java/dev/henriquesilva/loja_virtual/model/Person.java
@@ -1,8 +1,12 @@
 package dev.henriquesilva.loja_virtual.model;
 
 import jakarta.persistence.*;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
@@ -19,6 +23,9 @@ public abstract class Person implements Serializable {
     private String email;
     @Column(nullable = false, unique = true)
     private String phone;
+    @OneToMany(mappedBy = "person", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private List<Address> addresses = new ArrayList<Address>();
 
     public long getId() {
         return id;
