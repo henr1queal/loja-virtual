5b7f70cc6fcaf9027b6b4b190adfc2f108f5b589
Henrique Silva <henriquersilva.al@gmail.com>
2025-05-24 21:44:33 -0300

    Model Marca do Produto adicionado

---
+++ src/main/java/dev/henriquesilva/loja_virtual/model/ProductBrand.java
@@ -0,0 +1,46 @@
+package dev.henriquesilva.loja_virtual.model;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Table(name = "product_brand")
+@SequenceGenerator(name = "seq_product_brand", initialValue = 1, allocationSize = 1)
+public class ProductBrand implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_product_brand")
+    private long id;
+    private String brand;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        ProductBrand that = (ProductBrand) o;
+        return id == that.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
