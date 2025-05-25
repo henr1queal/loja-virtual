95e03cb99660eedd57ed50195507034006528c45
Henrique Silva <henriquersilva.al@gmail.com>
2025-05-24 22:50:04 -0300

    Access (GrantedAuthority) adicionado

---
+++ src/main/java/dev/henriquesilva/loja_virtual/model/Access.java
@@ -0,0 +1,47 @@
+package dev.henriquesilva.loja_virtual.model;
+
+import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "access")
+@SequenceGenerator(sequenceName = "seq_access", name = "seq_access", initialValue = 1, allocationSize = 1)
+public class Access implements GrantedAuthority {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_access")
+    private long id;
+
+    @Column(unique = true, nullable = false)
+    private String authority;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    @Override
+    public String getAuthority() {
+        return authority;
+    }
+
+    public void setAuthority(String authority) {
+        this.authority = authority;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Access access = (Access) o;
+        return id == access.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
